af76f1abfa3f0a5d3a7507319e52e802aa2ad164
Craig Campbell <craigaaroncampbell@gmail.com>
2016-05-03 10:42:50 -0700

    fix spaces

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,18 @@ const webpack = require('webpack-stream');
 gulp.task('default', ['build:dev']);
 
 gulp.task('static:dev', () => {
-	gulp.src(['app/**/*.html', 'app/css/**/*.css'])
-	.pipe(gulp.dest('build/'));
+  gulp.src(['app/**/*.html', 'app/css/**/*.css'])
+  .pipe(gulp.dest('build/'));
 });
 
 gulp.task('webpack:dev', () => {
-	gulp.src('app/js/entry.js')
-	.pipe(webpack({
-		output: {
-			filename: 'bundle.js'
-		}
-	}))
-	.pipe(gulp.dest('build/'));
+  gulp.src('app/js/entry.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('build/'));
 });
 
 gulp.task('build:dev', ['webpack:dev', 'static:dev']);
